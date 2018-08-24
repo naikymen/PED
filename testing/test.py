23e8d8910435cce10bae16de1a32d95062860168
Unknown <nico.fruta@gmail.com>
2018-08-24 13:13:24 +0200

    option parsing now works perfectly :)

--- testing/test.py
+++ testing/test.py
@@ -15,27 +15,62 @@ def prettyjson(diccionario):
     print(json.dumps(data, sort_keys=True, indent=4))
 
 
-# Start the option.parser and look for a configuration file
-usage = "usage: pedbPipe [options] [-l <entry list>] \
-XXXX PEDXXXX ensemble# conformer# ensemble1 ensemble2 ensemble3 ..."
-parser = OptionParser(usage)
-parser.add_option("-c", "--config", action="store",
-                  type="string", dest="config", default="",
-                  help="load options from a configuration file.")
-(options, args) = parser.parse_args()
+def updateDict(originalDict, modifierDict, defaultDict, omitKeys=[], omitValues=['default']):
+    # The original will be updated by the modified:
+    # Only if the modified is different from the default.
+    # Specific keys and values can be omitted from modification,
+    # by using the omitKeys and omitValues parameters.
+    for key in modifierDict.keys():
+        if key in omitKeys:
+            continue
+        try:
+            # Check if the keys in the modifier are present in the original
+            # An exception will be raised if the option is "unrecognized"
+            originalDict[key]
+
+            # Update the original if the modified is different from the default
+            # AND if the values are not in the omitions list
+            omitValues.append(defaultDict[key])
+            if modifierDict[key] not in omitValues:
+                originalDict[key] = modifierDict[key]
+        except KeyError as ke:
+            # https://docs.python.org/3/library/exceptions.html
+            print("\nERROR\nOption '%s' is not available." % key)
+            print("\nAvailable options in original are:")
+            print(originalDict.keys())
+            print("\nAvailable options in default are:")
+            print(defaultDict.keys())
+            print("\nOptions in modifier are:")
+            print(modifierDict.keys(), '\n')
+            raise ke
 
 
 # Default Options
 defaults = {
     "working_directory": os.getcwd(),
-    "list_input": None,
+    "list_input": "",
     "scripts": 'Scripts/',
     "pdb": "./",
     "saxs": "./",
 }
-print('Defaults')
-prettyjson(defaults)
+settings = {
+    "working_directory": os.getcwd(),
+    "list_input": "",
+    "scripts": 'Scripts/',
+    "pdb": "./",
+    "saxs": "./",
+}
 
+
+
+# Start the option.parser and look for a configuration file
+usage = "usage: pedbPipe [options] [-l <entry list>] \
+XXXX PEDXXXX ensemble# conformer# ensemble1 ensemble2 ensemble3 ..."
+parser = OptionParser(usage)
+
+parser.add_option("-c", "--config", action="store",
+                  type="string", dest="config", default="",
+                  help="load options from a configuration file.")
 
 # Let everything be overwritten by command-line options
 parser.add_option("-w", "--working_directory", action="store",
@@ -43,7 +78,7 @@ parser.add_option("-w", "--working_directory", action="store",
                   help="set the working directory.")
 
 parser.add_option("-l", "--list", action="store",
-                  type="string", dest="input", default=defaults["list_input"],
+                  type="string", dest="list_input", default=defaults["list_input"],
                   help="file from where to read several input entries.")
 
 parser.add_option("-p", "--scripts", action="store",
@@ -63,34 +98,23 @@ parser.add_option("-n", "--dry", action="store_true",
                   help="print input and exit.")
 
 (options, args) = parser.parse_args()
-
+# https://stackoverflow.com/questions/1753460/python-optparse-values-instance
 
 # Config file options: PARSE AS DEFINED IN THE "Default Options" section above
-if options.config != '':
-    with open(options.config, "r") as read_file:
-        data = json.load(read_file)
-        for key in data.keys():
-            try:
-                defaults[key]
-            except KeyError as ke:
-                # https://docs.python.org/3/library/exceptions.html
-                print("\nOption name '%s' in config file is not valid." % key)
-                print("Available options are:")
-                prettyjson(defaults)
-                raise ke
-            if data[key] != 'default':
-                    defaults[key] = data[key]
-print('Config')
-prettyjson(defaults)
+with open(options.config, "r") as read_file:
+    configopts = json.load(read_file)  # It is dict type
+# Parse the cli options
+cliopts = vars(options)
 
+# Update the configuration
+updateDict(settings, configopts, defaults)
+updateDict(settings, cliopts, defaults, omitKeys=['config', 'dry'])
 
 if options.dry is True:
     print("Dry run, printing options and exiting:")
-    prettyjson(vars(options))
+    prettyjson(defaults)
+    prettyjson(configopts)
+    prettyjson(cliopts)
+    prettyjson(settings)
     # https://stackoverflow.com/questions/19747371/python-exit-commands-why-so-many-and-when-should-each-be-used/19747562
     sys.exit()
-
-
-print('Options')
-# https://stackoverflow.com/questions/1753460/python-optparse-values-instance
-prettyjson(vars(options))
