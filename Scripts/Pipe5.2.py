4491411086d9d6a28723bf2913371c64e6c138bc
Unknown <nico.fruta@gmail.com>
2018-08-27 15:27:30 +0200

    exit on error flag for pymol script

--- Scripts/Pipe5.2.py
+++ Scripts/Pipe5.2.py
@@ -1,7 +1,7 @@
 # A python 2 script for controlling PyMol
 
 import __main__
-__main__.pymol_argv = ['pymol', '-qc'] # Pymol: quiet and no GUI
+__main__.pymol_argv = ['pymol', '-qcy'] # Pymol: quiet and no GUI
 from time import sleep
 import pymol
 from pymol.cgo import *
